f0b31ef1f618ae153a88ad58158be43e75d063b2
Juliusfrank11 <43428735+Juliusfrank11@users.noreply.github.com>
2019-09-02 16:09:56 -0400

    Update probability-of-stopping.py
    Fixed definition of p
    
    Optimized code by eliminating unnecessary for loops

--- probability-of-stopping.py
+++ probability-of-stopping.py
@@ -1,17 +1,18 @@
-import more_itertools
-# The basic idea behind this code is that we will first generate all w-permutations of "S" and "F" to a list, then remove ones which do not make sence in the context of the problem
+from collections import Counter
+import math
+# The basic idea behind this code is that we will first generate all w-permutations of "S" and "F" to a list that fit the criteria of the problem
 # w = time periods
 # n = number of successes required before experiment end not allowed
-# p = probability of failure
+# p = probability of success
 # s = number of successes in a general permutation
 # f = w - s, number of failures in a general permutation
 
-# Change these to suit your needs
-w = 12
+# change these as you like
+w = 15
 n = 6
 p = 3/8
 
-# Generate substrings that we should not see in valid permutations
+#Generate substrings that we should not see in the permutation ('F' followed by less than n successes)
 forbidden_substrings = []
 for i in range(0, n):
     forbidden_substring = 'F'
@@ -19,11 +20,12 @@ for i in range(0, n):
         forbidden_substring += 'S'
     forbidden_substring += 'F'
     forbidden_substrings.append(forbidden_substring)
-forbidden_substrings.append('FFF')
 
+#Convert list to string in order to use .endswith
 def convert(li):
     return ''.join(li)
 
+#Create an example permutation with s Ss and f Fs
 def create_permutation(s):
     elements = []
     for i in range(0, s):
@@ -32,20 +34,14 @@ def create_permutation(s):
         elements.append('F')
     return elements
 
+#Count now many times each element of an iterable is in a string
 def count_for_iterables(string, iterable):
     count = 0
     for element in iterable:
         count += string.count(element)
     return count
 
-def element_in_string(string, iterable):
-    if count_for_iterables(string, iterable) > 1 or count_for_iterables(string, iterable) == 0:
-        return None
-    else:
-        for element in iterable:
-            if element in string:
-                return element
-
+#Return the max index of a character in a string up to index w-n-1
 def max_index_of_character(string, character):
     if character not in string:
         return -1
@@ -57,52 +53,42 @@ def max_index_of_character(string, character):
                     break
         return max_index
 
-def find_valid_permutations(permutations):
-    valid_permutations = []
-    for permutation in permutations:
-        # make sure there is only one instance of a forbidden substring in a stopping permutaton
-        while count_for_iterables(permutation, forbidden_substrings) > 1:
-            for forbidden_substring in forbidden_substrings:
-                if forbidden_substring in permutation:
-                    permutation = permutation[0:permutation.index(forbidden_substring)+len(forbidden_substring)]
-        valid_permutations.append(permutation)
-        if element_in_string(permutation, forbidden_substrings) != None:
-            el = element_in_string(permutation, forbidden_substrings)
-            #The forbidden string should be at the end of the permuation and we should remove things like FSSFSSF when n > 2 even though count_for_iterables will return one with such a string
-            if not permutation.endswith(el) or (permutation.index(el) < len(permutation) - len(el)):
-                valid_permutations.remove(permutation)
-    valid_permutations_temp = valid_permutations
-    for valid_permutation in valid_permutations_temp:
-        # Processes should stop if there is a failure at least on period w-n
-        if max_index_of_character(valid_permutation, 'F') > w-n-1 and max_index_of_character(valid_permutation, 'F') != w-1:
-            valid_permutations[valid_permutations.index(valid_permutation)] = valid_permutation[:max_index_of_character(valid_permutation, 'F') + 1]
-        #All successes should be of lenght w
-        elif not valid_permutation.endswith('F') and len(valid_permutation) < w:
-            valid_permutation[valid_permutations.index(valid_permutation)] = valid_permutation+ 'F'
-    #remove duplicates
-    valid_permutations = list(set(valid_permutations))
-    return valid_permutations
+#Code adapted from more_itertools' distinct_permuation function
+def distinct_success_permutations(iterable):
+    def perm_unique_helper(item_counts, perm, i):
+        if i < 0:
+            yield tuple(perm)
+        else:
+            for item in item_counts:
+                if item_counts[item] <= 0:
+                    continue
+                perm[i] = item
+                item_counts[item] -= 1
+                for x in perm_unique_helper(item_counts, perm, i - 1):
+                    permutation = convert(x)
+                    #Make sure it's a success
+                    if permutation.endswith('S'):
+                         # make sure there are zero instances of a forbidden substring in a success permutaton and that there is no failure afte w-m-1 trails
+                        if not count_for_iterables(permutation, forbidden_substrings) > 0 and not max_index_of_character(permutation, 'F') > w-n-1:
+                            yield permutation
+                item_counts[item] += 1
 
-def find_probability(permutation):
-    s = permutation.count('S')
-    f = permutation.count('F')
-    probability = ((1-p)**s)*(p**f)
-    return probability
+    item_counts = Counter(iterable)
+    length = sum(item_counts.values())
 
-def probability_of_stopping(permutations):
-    stopping_probability = 0
-    for permutation in permutations:
-        if permutation.endswith('F'):
-            stopping_probability += find_probability(permutation)
-    return stopping_probability
+    return perm_unique_helper(item_counts, [None] * length, length - 1)
 
 def main():
-    valid_permutations = []
-    for i in range(0, w+1):
-        permutation = create_permutation(i)
-        permutations = list(map(convert, more_itertools.distinct_permutations(permutation)))
-        valid_permutations += find_valid_permutations(permutations)
-    valid_permutations = list(set(valid_permutations))
-    print('The probability of stopping: ', probability_of_stopping(valid_permutations))
-    
+    success_permutations = []
+    # theoretically impossible for there to be more than floor(w/2) failures in a success permutation
+    for s in range(math.floor(w/2), w+1):
+        success_permutations += list(distinct_success_permutations(create_permutation(s)))
+    success_permutations = list(set(success_permutations))
+    probability_of_success = 0
+    for success_permutation in success_permutations:
+        s = success_permutation.count('S')
+        probability_of_success += ((p)**s)*((1-p)**(w-s))
+    probability_of_failure = 1 - probability_of_success
+    print('The probability of failure is: ', probability_of_failure)
+
 main()
